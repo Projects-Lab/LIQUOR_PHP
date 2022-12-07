9ad0607e432d1e7313c86d8e1bade49dd322dab3
Brasil_Campeon_2022 <guillermobedoya104@gmail.com>
2022-12-07 16:59:02 -0500

    Inventario

--- inventario.php
+++ inventario.php
@@ -16,11 +16,9 @@
 
     <!-- component -->
 
-
     <?php
     include "navbar.php";
     ?>
-
 
 
     <section class="text-gray-600 body-font">
@@ -32,8 +30,8 @@
                         <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://firebasestorage.googleapis.com/v0/b/thecaffeinecode.appspot.com/o/blog.jpg?alt=media&token=271cb624-94d4-468d-a14d-455377ba75c2" alt="blog cover" />
                         <div class="p-4">
                             <div class="flex items-center flex-wrap ">
-                                <a href="inventario.php" class="text-green-800  md:mb-2 lg:mb-0">
-                                    <p class="inline-flex items-center">Inventario
+                                <a href="/" class="text-green-800  md:mb-2 lg:mb-0">
+                                    <p class="inline-flex items-center">Categorías
                                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path d="M5 12h14"></path>
                                             <path d="M12 5l7 7-7 7"></path>
@@ -45,6 +43,23 @@
                     </div>
                 </div>
 
+                <div class="p-4 md:w-1/3">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://firebasestorage.googleapis.com/v0/b/thecaffeinecode.appspot.com/o/blog.jpg?alt=media&token=271cb624-94d4-468d-a14d-455377ba75c2" alt="blog cover" />
+                        <div class="p-4">
+                            <div class="flex items-center flex-wrap ">
+                                <a href="/" class="text-green-800  md:mb-2 lg:mb-0">
+                                    <p class="inline-flex items-center">Proveedores
+                                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M5 12h14"></path>
+                                            <path d="M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="p-4 md:w-1/3">
                     <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
@@ -52,7 +67,7 @@
                         <div class="p-4">
                             <div class="flex items-center text-center flex-wrap ">
                                 <a href="/" class="text-green-800  md:mb-2 lg:mb-0">
-                                    <p class="inline-flex items-center">Ventas
+                                    <p class="inline-flex items-center">Productos
                                         <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path d="M5 12h14"></path>
                                             <path d="M12 5l7 7-7 7"></path>
