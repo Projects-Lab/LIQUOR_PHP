39caff67bf8e6f0c116936677775e4916eb6d670
Carlos Villadiego <carlovinar93@gmail.com>
2022-12-07 22:38:58 -0500

    se creo el archivos mostrar.php

--- proveedores.php
+++ proveedores.php
@@ -39,6 +39,10 @@
             include('registrar_proveedor.php');
         ?>
 
+        <?php
+            include('mostrar_proveedor.php');
+        ?>
+
       
 
 
@@ -47,7 +51,7 @@
             <!-- Table -->
             <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
                 <header class="px-5 py-4 border-b border-gray-100">
-                    <h2 class="font-semibold text-gray-800">Customers</h2>
+                    <h2 class="font-semibold text-gray-800">Lista de proveedores</h2>
                 </header>
                 <div class="p-3">
                     <div class="overflow-x-auto">
