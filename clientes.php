7f87b0a0c9403c08e283b9a5eb4a2accb403c5e6
Guillotina <guillermobedoya104@gmail.com>
2022-12-08 17:07:49 -0500

    Modulo de Ventas OK

--- clientes.php
+++ clientes.php
@@ -19,28 +19,33 @@
     <?php
     include "navbar.php";
     ?>
-    <div class="flex flex-wrap justify-evenly my-20">
+    <div class="mt-12 flex flex-wrap justify-around ">
     <div class="flex-col-reverse text-4xl ">
-                        <div>
-                            <a href="javascript: history.go(-1)">
-                                <ion-icon name="arrow-undo-circle-outline"></ion-icon>
-                            </a>
-                        </div>
-                    </div>
-
-        <form class=" w-80 rounded flex justify-center items-center flex-col shadow-md">
-            <p class="mb-2 text-3xl text-gray-600">Crear Categoria</p>
-            <div class="flex my-2 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
-                <input class="text-center w-full py-2 pl-2 md:pl-8 border-0 focus:outline-none" placeholder="categoria" type="text" required>
+            <div>
+                <a href="javascript: history.go(-1)">
+                    <ion-icon name="arrow-undo-circle-outline"></ion-icon>
+                </a>
             </div>
-            <button class="my-3 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-800 dark:focus:ring-green-900" id="login" type="submit"><span>Crear</span></button>
-        </form>
+        </div>
 
+        <form action="#" class="w-80 rounded flex justify-center items-center flex-col shadow-md" method="post">
+            <p class="mb-2 text-3xl text-gray-600">Crear Clientes</p>
+            <div class="flex my-2 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
+                <input class="text-center w-full py-2 pl-2 md:pl-8 border-0 focus:outline-none" placeholder="Nombre" type="text" name="nombre_cliente" required>
+            </div>
+            <div class="flex my-2 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
+                <input class="text-center w-full py-2 pl-2 md:pl-8 border-0 focus:outline-none" placeholder="Apellido" type="text" name="apellido_cliente" required>
+            </div>
+            <div class="flex my-2 mx-4 md:mx-2 border-b-2 border-gray-700 hover:border-green-800">
+                <input class="text-center w-full py-2 pl-2 md:pl-8 border-0 focus:outline-none" placeholder="Telefono" type="number" name="telefono_cliente" required>
+            </div>
+            <button class="my-3 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-800 dark:focus:ring-green-900" type="submit"><span>Crear</span></button>
+        </form>
 
         <div>
             <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
                 <header class="px-5 py-4 border-b border-gray-100">
-                    <h2 class="font-semibold text-gray-800">Lista de Categorias</h2>
+                    <h2 class="text-center font-semibold text-gray-800">Lista de Clientes</h2>
                 </header>
                 <div class="p-3">
                     <div class="overflow-x-auto">
@@ -48,7 +53,14 @@
                             <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
                                 <tr>
                                     <th class="p-2 whitespace-nowrap">
-                                        <div class="font-semibold text-left">Nombre Categoria</div>
+                                        <div class="font-semibold text-left">Nombre</div>
+                                    </th>
+
+                                    <th class="p-2 whitespace-nowrap">
+                                        <div class="font-semibold text-left">Apellido</div>
+                                    </th>
+                                    <th class="p-2 whitespace-nowrap">
+                                        <div class="font-semibold text-left">Telefono</div>
                                     </th>
                                     <th class="p-2 whitespace-nowrap">
                                         <div class="font-semibold text-center">Acciones</div>
@@ -59,8 +71,14 @@
                                 <tr>
                                     <td class="p-2 whitespace-nowrap">
                                         <div class="flex items-center">
-                                            <div class="font-medium text-gray-800">Alex Shatov</div>
+                                            <div class="font-medium text-gray-800"> Pablo </div>
                                         </div>
+                                    </td>
+                                    <td class="p-2 whitespace-nowrap">
+                                        <div class="text-left font-medium text-green-500">Chacon</div>
+                                    </td>
+                                    <td class="p-2 whitespace-nowrap">
+                                        <div class="text-left">3213213213</div>
                                     </td>
                                     <td class="p-2 whitespace-nowrap">
                                         <div class="text-lg text-center">
@@ -79,11 +97,10 @@
                 </div>
             </div>
         </div>
-        <div class="flex justify-end text-4xl ">
-            <div class="text-4xl flex space-x-4">
-                <ion-icon name="exit-outline"></ion-icon>
-            </div>
+        <div class="text-4xl flex space-x-4">
+            <ion-icon name="exit-outline"></ion-icon>
         </div>
+
     </div>
 
     <?php
---
+++ scripts.php
@@ -0,0 +1,3 @@
+<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+<script src="https://kit.fontawesome.com/8a4af9b6ef.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
